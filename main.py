596aac3b11cd8707515d0e92207e466ca6fe3560
fariau <usmanfaria88@gmail.com>
2025-07-26 10:01:24 +0500

    done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# AI Travel Designer Agent
+
 import os
 from dotenv import load_dotenv
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
@@ -17,12 +19,14 @@ destination_agent = Agent(
     instructions="You recommend a travel destination based on the user's mood.",
     model=model
 )
+
 booking_agent = Agent(
     name="BookingAgent",
     instructions="You provide flight and hotel information using the available tools.",
     model=model,
     tools=[get_flights, suggest_hotels]
 )
+
 explore_agent = Agent(
     name="ExploreAgent",
     instructions="You suggest food and attractions to explore at the destination.",
@@ -30,18 +34,19 @@ explore_agent = Agent(
 )
 
 def main():
-    print("AI Travel Designer Agent\n")
-    mood = input("Whats your travel mood (relaxing/advanture/etc)? ")
+    print("\n🌍✈️  AI Travel Designer Agent 🧳✨\n")
+    
+    mood = input("😌  What's your travel mood? (relaxing / adventure / etc): ")
     
     result1 = Runner.run_sync(destination_agent, mood, run_config=config)
     dest = result1.final_output.strip()
-    print("Destination suggest:", dest)
+    print("\n📍  Destination Suggestion:  🌟", dest)
     
     result2 = Runner.run_sync(booking_agent, dest, run_config=config)
-    print("Booking Info:", result2.final_output)
+    print("\n🛏️ ✈️  Booking Info:\n", result2.final_output)
     
     result3 = Runner.run_sync(explore_agent, dest, run_config=config)
-    print("Explore Tips:", result3.final_output)
+    print("\n🍽️ 🗺️  Explore Tips:\n", result3.final_output)
     
 if __name__ == "__main__":
     main()
